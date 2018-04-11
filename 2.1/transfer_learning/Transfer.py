da16501611eeeecc56c69e3bf60327952c453d11
Peter Reutemann <fracpete@gmail.com>
2018-04-11 17:01:27 +1200

    fixed typos

--- 2.1/transfer_learning/Transfer.py
+++ 2.1/transfer_learning/Transfer.py
@@ -276,14 +276,14 @@ def create_model(base_model_file, feature_node_name, last_hidden_node_name, num_
     if feature_node is None:
         feature_node = find_by_uid(base_model, feature_node_name)
     if feature_node is None:
-        raise Exception("Failed to located feature node: " + feature_node_name)
+        raise Exception("Failed to locate feature node: " + feature_node_name)
     print("feature node", feature_node)
     # last hidden node
     last_node = find_by_name(base_model, last_hidden_node_name)
     if last_node is None:
         last_node = find_by_uid(base_model, last_hidden_node_name)
     if last_node is None:
-        raise Exception("Failed to located last hidden node: " + last_hidden_node_name)
+        raise Exception("Failed to locate last hidden node: " + last_hidden_node_name)
     print("last hidden node", last_node)
 
     # Clone the desired layers with fixed weights
